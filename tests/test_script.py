9178fccb861d5bfab94f7ddafa0f4847da7c7a0e
ajanthav10 <ajanthav10@gmail.com>
2023-01-02 16:01:37 -0700

    worked  on chpc and script

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,8 +1,10 @@
-from os import system as cmd_line
+from os import system 
 from typing import List, Union as Un, Dict
 from pathlib import Path
 from dataclasses import dataclass
+import sys
 
+DEBUG=True
 @dataclass
 class GeneratorConfigOption:
     value:str
@@ -30,23 +32,23 @@ MCO = ModelCheckerConfigOption
 CC = "gcc"
 CXX = "g++"
 
-CXXPARAMS = "-std=c++17 -O3 -pthread"
-CCPARAMS ="-march=native -O3 -lpthread"
+CXX_PARAMS = "-std=c++17 -O3 -pthread"
+CC_PARAMS ="-march=native -O3 -lpthread"
 
 SAVE_PATH = "./submit_jobs.sh"
-ROMP = None #TODO: path to romp executable from planned cwd or machines root
+ROMP = "../build/romp/romp"
 ROMP_PARAMS:Params_t = {"symmetry":[None,GCO('-s')],"trace":[MCO('-t'),None],"trace-comp":[GCO('--simple-trace-re')]} 
-CMURPHI = None #TODO: path to cmurphi executable from planned cwd or machines root
+CMURPHI = "../../cmurphi/src/mu"
 CMURPHI_PARAMS:Params_t = {"HashCompaction":[GCO("-c"),None],"BitCompaction":[GCO("-b"),None],"MemLimit":[MCO("-m10000")],"trace-DFS":[MCO("ON"),MCO("OFF")]} #TODO make this option match cmurphi man/help page
-RUMUR = None #TODO: path to rumur executable from planned cwd or machines root
+RUMUR = "../../rumur/build/rumur/rumur"
 RUMUR_PARAMS:Params_t = {"symmetry":[GCO("--symetry-reduction="+i) for i in ["heustric", "exhaustive", "off"]],
                         "bound":["default",10000], #TODO make this option match rumur man/help page
                         "trace-DFS":["ON","OFF"]}  #TODO make this option match rumur man/help page
 
 SBATCH_PARMAS:str = "-M kingspeak --account=ganesh --partition=kingspeak-shared --nodes=1 --ntasks=1 -C c16 -c 16 --exclusive --time=20:00:00 --mail-type=FAIL,BEGIN,END --mail-user=**0@gmail.com"
 
-ALL_MODELS: List[str] = [
-        #TODO past in all file dirs
+ALL_MODELS: List[str] = ["./german.m"
+        
     ]
 # SPIN_PARAMS  = {} # TODO: add this when we work with everything else
 
@@ -64,108 +66,108 @@ ALL_MODELS: List[str] = [
 class ConfigGenerator:
     """To generate the configuration for the testing ROMP"""
     def __init__(self, modgen:str, comp:str, comp_params:str, params:Params_t, models:List[str], exe_ext:str=None) -> None:
-        self.__modgen: str = modgen
-        self.__exe_ext: str = Path(modgen).stem if exe_ext is None else exe_ext
-        self.__src_ext: str = ".cpp" if "++" in comp else ".c"
-        self.__comp: str = comp
-        self.__comp_params: str = comp_params
-        self.__PARAMS: Params_t = dict(params)
-        self.__models: List[Path] = [Path(i) for i in models]
-        self.__param_keys: List[str] = self.__PARAMS.keys().sort()
-        self.__index: int = None
-        self.__i: int = None # model
-        self.__j: int = None # param key
-        self.__k: int = None # param value
-        self.__config: Config_t = None
-        self.__len:int = self.__calc_len()
+        self._modgen: str = modgen
+        self._exe_ext: str = Path(modgen).stem if exe_ext is None else exe_ext
+        self._src_ext: str = ".cpp" if "++" in comp else ".c"
+        self._comp: str = comp
+        self._comp_params: str = comp_params
+        self._PARAMS: Params_t = dict(params)
+        self._models: List[Path] = [Path(i) for i in models]
+        self._param_keys: List[str] = (list(self._PARAMS.keys())).sort()
+        self._index: int = None
+        self._i: int = None # model
+        self._j: int = None # param key
+        self._k: int = None # param value
+        self._config: Config_t = None
+        self._len:int = self._calc_len()
     
         
     @property
     def PARAMS(self) -> dict:
-        return self.__PARAMS
+        return self._PARAMS
     @property
     def index(self) -> int:
-        return self.__index
+        return self._index
     
-    def __create_config(self) -> None:
-        self.__config = { key: l[0] for (key,l) in self.__PARAMS.items() }
+    def _create_config(self) -> None:
+        self._config = { key: l[0] for (key,l) in self._PARAMS.items() }
     
-    def __update_config(self,i,j,k) -> None:
-        self.__config[self.__param_keys[j]] = self.__PARAMS[self.__param_keys[j]][k]
+    def _update_config(self,i,j,k) -> None:
+        self._config[self._param_keys[j]] = self._PARAMS[self._param_keys[j]][k]
     
     @property
     def config(self) -> dict:
-        if self.__config is None:
+        if self._config is None:
             raise Exception("config not generated")
-        return self.__config
+        return self._config
     
    
 
     @property
     def gen_cmd(self) -> str:
-        if self.__index is None:
+        if self._index is None:
             raise Exception("ConfigGenerator not in iterator mode!!")
-        base_model = self.__models[self.__i].with_suffix('')
-        other_opts = [i.value for i in self.__config.values() if isinstance(i,GeneratorConfigOption)].join(' ')
-        return f"{self.__modgen} {other_opts} -o {base_model}.{self.__src_ext}"
+        base_model = self._models[self._i].with_suffix('')
+        other_opts = [i.value for i in self._config.values() if isinstance(i,GeneratorConfigOption)].join(' ')
+        return f"{self._modgen} {other_opts} -o {base_model}.{self._src_ext}"
         # note CMurphy does not accept -o or any output config details (because its annoying and poorly made)
 
     
     @property
     def comp_cmd(self) -> str:
-        if self.__index is None:
+        if self._index is None:
             raise Exception("ConfigGenerator not in iterator mode!!")
-        base_model = self.__models[self.__i].with_suffix('')
-        other_opts = [i.value for i in self.__config.values() if isinstance(i,CompilerConfigOption)].join(' ')
-        return f"{self.__comp} {self.__comp_params} {other_opts} -o {base_model}.{self.__exe_ext} {base_model}.{self.__src_ext}"
+        base_model = self._models[self._i].with_suffix('')
+        other_opts = [i.value for i in self._config.values() if isinstance(i,CompilerConfigOption)].join(' ')
+        return f"{self._comp} {self._comp_params} {other_opts} -o {base_model}.{self._exe_ext} {base_model}.{self._src_ext}"
     
     
     def sbatch_cmd(self,slurmOpts:str) -> str:
-        if self.__index is None:
+        if self._index is None:
             raise Exception("ConfigGenerator not in iterator mode!!")
-        base_model = self.__models[self.__i].with_suffix('')
-        launch_opts = [i.value for i in self.__config.values() if isinstance(i, ModelCheckerConfigOption)].join(' ')
-        return f"sbatch {slurmOpts} {base_model}.{self.__exe_ext} {self.__config.values().join(' ')}"
+        base_model = self._models[self._i].with_suffix('')
+        launch_opts = [i.value for i in self._config.values() if isinstance(i, ModelCheckerConfigOption)].join(' ')
+        return f"sbatch {slurmOpts} {base_model}.{self._exe_ext} {self._config.values().join(' ')}"
 
-    def __calc_len(self) -> int:
+    def _calc_len(self) -> int:
         _len = 1
-        for values in self.__PARAMS.values():
+        for values in self._PARAMS.values():
             _len *= len(values)
-        self.__len = _len
-        return self.__len
+        self._len = _len
+        return self._len
     
     def __len__(self) -> int:
-        return self.__len
+        return self._len
     def __iter__(self):
-        self.__index = 0
-        self.__i = 0
-        self.__j = 0
-        self.__k = 0
-        self.__create_config()
+        self._index = 0
+        self._i = 0
+        self._j = 0
+        self._k = 0
+        self._create_config()
         return self
     def __next__(self):
-        if self.__index is None:
+        if self._index is None:
             raise Exception("ConfigGenerator not in iterator mode!!")
-        if self.__index >= len(self) or self.__index < 0:
+        if self._index >= len(self) or self._index < 0:
             raise StopIteration
-        self.__index += 1
-        self.__k += 1
-        if self.__k < len(self.__PARAMS[self.__PARAMS.keys[j]]):
+        self._index += 1
+        self._k += 1
+        if self._k < len(self.PARAMS[self.PARAMS.keys[self._j]]):
             pass   
         else:
-            self.__k = 0
-            self.__j += 1
-            if self.__j < len(self.__PARAMS):
+            self._k = 0
+            self._j += 1
+            if self._j < len(self.PARAMS):
                 pass
             else:
-                self.__update_config(self.__i,self.__j,self.__k)
-                self.__j = 0
-                self.__i += 1
-                if self.__i < len(self.__models):
+                self._update_config(self._i,self._j,self._k)
+                self._j = 0
+                self._i += 1
+                if self._i < len(self._models):
                     pass
                 else:
-                    self.__i = 0
-        self.__update_config(self.__i,self.__j,self.__k)
+                    self._i = 0
+        self._update_config(self._i,self._j,self._k)
         return self      
 
 
@@ -192,16 +194,22 @@ class ConfigGenerator:
 
 def launch(cg:ConfigGenerator,slurmOpts:str) -> None:
     for i in cg:
-        gen_cmd = cg.gen_cmd
-        cmd_line.system(gen_cmd)
+        gen_cmd = i.gen_cmd
+        print("Running :",gen_cmd)
+        if not DEBUG:
+            system(gen_cmd)
 
         # Compiling the model
-        comp_cmd = cg.comp_cmd
-        cmd_line.system(comp_cmd)
+        comp_cmd = i.comp_cmd
+        print("Running :",comp_cmd)
+        if not DEBUG:
+            system(comp_cmd)
 
         # Launching  model 
-        sbatch_cmd = cg.sbatch_cmd(slurmOpts)
-        cmd_line.system(sbatch_cmd)
+        sbatch_cmd = i.sbatch_cmd(slurmOpts)
+        print("Running :",sbatch_cmd)
+        if not DEBUG:
+            system(sbatch_cmd)
 
 def main(args)-> None:
     romp_configs = ConfigGenerator(ROMP,CXX,CXX_PARAMS,ROMP_PARAMS,ALL_MODELS,".romp")
@@ -215,4 +223,4 @@ def main(args)-> None:
     launch(rumur_configs,SBATCH_PARMAS)
    
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
