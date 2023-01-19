0f369ce3d26b7121e2c164a6884073cf50a01ddd
ajanthav10 <ajanthav10@gmail.com>
2023-01-19 13:50:56 -0700

    added

---
+++ tests/test_crunching.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+
+
+def main()->None:
+    '''Things to do 
+    1) open the file in the path ".\relative ?"
+        open all .txt files in loop
+            
+    '''
+
+if __name__ == "__main__":
+    main()
