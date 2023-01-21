473bf0ed57b0a4c1d37987e6f6791ba7f9cf1386
ajanthav10 <ajanthav10@gmail.com>
2023-01-20 17:17:27 -0700

    Added regex

--- tests/test_crunching.py
+++ tests/test_crunching.py
@@ -1,5 +1,6 @@
 #using pandas then converting it into a csv ?
 import pandas as pd
+import re
 import os
 path = "Path"
 #changing the current working directory to the provided path
@@ -15,6 +16,34 @@ def romp(f):
 
 def cmurphi(f):
     pass
+import re
+
+def rumur(f):
+    lines = f.readlines()
+    memory_usage = lines[0]
+    progress_report = lines[1]
+    status = lines[3]
+    state_space_explored = lines[4]
+
+    size_of_state = int(re.search(r'(\d+) bits', memory_usage).group(1))
+    size_of_hash_table = int(re.search(r'(\d+) slots', memory_usage).group(1))
+
+    states_explored = int(re.search(r'(\d+) states explored', progress_report).group(1))
+    rules_fired = int(re.search(r'(\d+) rules fired', progress_report).group(1))
+    time_taken = float(re.search(r'(\d+\.\d+)s', progress_report).group(1))
+
+    error_status = re.search(r'Status:(.+)', status).group(1).strip()
+
+    states = int(re.search(r'(\d+) states', state_space_explored).group(1))
+    rules_fired = int(re.search(r'(\d+) rules', state_space_explored).group(1))
+    time_taken = float(re.search(r'(\d+\.\d+)s', state_space_explored).group(1))
+
+    data = {'model':'rumur', 'States_visited': states_explored, 'Rules_Fired': rules_fired, 'Time': time_taken,
+            'Memory': size_of_state, 'Hashtable_Size': size_of_hash_table, 'Error_Status':error_status}
+
+    stats_df = stats_df.append(data, ignore_index=True)
+
+    return stats_df
 
 
 def rumur(f):
@@ -71,7 +100,25 @@ def read_text_file(path) -> None:
                 else:
                     cmurphi(f)
 
-
+def fs_DFS(path, dirCallback=None, fileCallback=None) -> List[str]:
+    if not os.path.exists(path):
+        raise Exception("Path does not exist!!")
+    stack = []
+    ret = []
+    stack.append(path);
+    while len(stack) > 0:
+        tmp = stack.pop(len(stack) - 1)
+        if(os.path.isdir(tmp)):
+            ret.append(tmp)
+            for item in os.listdir(tmp):
+                stack.append(os.path.join(tmp, item))
+            if dirCallback:
+                dirCallback(tmp)
+        elif(os.path.isfile(tmp)):
+            ret.append(tmp)
+            if fileCallback:
+                fileCallback(tmp)
+    return ret
 
 def main()->None:
     '''Things to do 
@@ -85,12 +132,9 @@ def main()->None:
             what are subdata to be stored
 
     '''
-    cols=["model","rumur_States_visited","rumur_Rules_Fired","rumur_Time","rumur_Memory","rumur_HashtableSize","rumur_Error_status",""]# todo add cols name for romp and cm
-    data ={'model','rumur': {'States_visited': [], 'Rules_Fired': [], 'Time': [], 'Memory':[],'Hashtable_Size': [],"Error_Status":[]},
-            'romp': {'States_visited': [], 'time': [], 'memory': []},
-            'cmurphi': {'States_visited': [], 'time': [], 'memory': []}}
-            #TODO finish the cols name with respect to flags and other data
-    stats_df=pd.DataFrame(data,columns=cols)
+
+    cols=["Model_name","Config_details","Error_status","States Generated","Time","Rules_Fired","ModelChecker_type","Valgrind_data"]# todo add cols name for romp and cm
+    stats_df=pd.DataFrame(columns=cols)
     #created a pd dataframe with subcols each for flag + others 
     # call opening file function in loop 
     read_text_file()
