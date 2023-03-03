34eee89527e0f414ba3783206095b1e401d078d1
ajanthav10 <87161640+ajanthav10@users.noreply.github.com>
2023-03-03 11:51:18 -0700

    made changes for cmurphi

--- tests/test_crunching.py
+++ tests/test_crunching.py
@@ -10,13 +10,79 @@ from gen_test_scripts import ROMP_CONFIGS, CMURPHI_CONFIGS, RUMUR_CONFIGS
 #changing the current working directory to the provided path
 os.chdir(path)
 
+
+ROMP_PATTERN = re.compile(r"""(?:.|\s)+
+(?<completed>Status:
+
+	(?:(?<no_error>No) error|(?<errors>\d+) error\(s\)|(?<deadlock>[Dd]eadlock)(?:\(s\))?) found.
+
+State Space Explored:
+
+	(?<states>\d+) states, (?<rules>\d+) rules fired in \d+s.
+)?
+ 
+Omission Probabilities (caused by Hash Compaction):
+
+    Pr\[even one omitted state\]\s+<=\s+\d+\.\d+\n\tPr\[even one undetected error\]\s+<=\s+\d+\.\d+\n\tDiameter of reachability graph: \d+$ #TODO
+
+TIME_NS=(?<time_ns>\d+)
+""")
+
+
 def romp(f):
     '''1) read the file data 
     '''
     pass
 
-def cmurphi(f):
-    pass
+CMURPHI_PATTERN = re.compile(r"""(?:.|\s)+
+(?<completed>Status:
+
+	(?:(?<no_error>No) error|(?<errors>\d+) error\(s\)|(?<deadlock>[Dd]eadlock)(?:\(s\))?) found.
+
+State Space Explored:
+
+	(?<states>\d+) states, (?<rules>\d+) rules fired in \d+s.
+)?
+ 
+Omission Probabilities (caused by Hash Compaction):
+
+    Pr\[even one omitted state\]\s+<=\s+\d+\.\d+\n\tPr\[even one undetected error\]\s+<=\s+\d+\.\d+\n\tDiameter of reachability graph: \d+$ #TODO
+
+TIME_NS=(?<time_ns>\d+)
+""")
+
+def cmurphi(f)->pd.DataFrame:
+    filepath = os.path(f)
+    index = int(str(filepath.stem).split('-')[0])
+    config_gen = CMURPHI_CONFIGS[index]
+    config = config_gen.config
+    model = str(config_gen.model.stem)
+    content = ""
+    with open(f, 'r') as f:
+        content = f.read()
+    if content == "":
+        raise Exception(f"File Contents unreadable ({f})")
+    # Extract the relevant information using regular expressions
+    '''searching for a pattern in string
+        if found 
+            get capture group
+            re.search() --gets match obj'''
+    m = CMURPHI_PATTERN.search(content)
+    data = {
+        "Model_name": model,
+        "Config_details": config,
+        "Completed": m.group('completed') is not None,
+        "Error_status": ('NO ERROR' if m.group('no_error') is not None else (
+                            int(m.group('errors')) if m.group('errors') is not None else (
+                            'DEADLOCK' if m.group('deadlock') is not None else None))),
+        "States Generated": int(m.group('states')) if m.group('states') is not None else None,
+        "Time": int(m.group('time_ns')) if m.group('time_ns') is not None else None,
+        "Rules_Fired": int(m.group('rules')) if m.group('rules') is not None else None,
+        "ModelChecker_type": 'cmuprhi',#TODO addition for analysis + Omission Probabilities ??
+        "Valgrind_data": None 
+        }
+    stats_df = stats_df.append(data, ignore_index=True)
+    return stats_df
 
 RUMUR_PATTERN = re.compile(r"""(?:.|\s)+
 (?<completed>Status:
@@ -92,6 +158,7 @@ def main()->None:
 
     '''
     cols=["Model_name","Config_details","Completed","Error_status","States Generated","Time","Rules_Fired","ModelChecker_type","Valgrind_data"]# todo add cols name for romp and cm
+    //do col name to be added for cmurphi - omission prob stats required and analysis of state space  // -pr gives rules information is it required ?
     stats_df=pd.DataFrame(columns=cols)
     #created a pd dataframe with subcols each for flag + others 
     # call opening file function in loop 
