3c9854f9b9cad2739554693cc7d45f8f71e87f59
osterhoutan-UofU <andrew.osterhout@utah.edu>
2023-04-11 15:47:07 -0700

    worked on ploting script

--- tests/plot_test_data.py
+++ tests/plot_test_data.py
@@ -1,32 +1,35 @@
-import argparse
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
 
+# %%
+import pandas as pd
+# import matplotlib.pyplot as plt
+import numpy as np
+#import seaborn as sns
+
+#%%
 def violin_plot():
     #TODO
+    pass
 
-def box_plot(df_selected, model, feature):
+def box_plot(df_selected):
+    """
+    Take in a DF selection in the form of `{'group': {'label': [data-points], more label groups ...}, more groups...}`
+    
+    """
     #TODO printing plots
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("model_name",help="Provide the model_name ")
-    parser.add_argument("feature", help="provide the feature")
-    return parser.parse_args()
 
 def main():
-    args = get_args()
-    model = args.model_name
-    feature =args.feature
-    df1 = pd.read_csv("./data/23-03-30_16-21-39_results.tsv",delimiter='\t')
-    df2 = pd.read_csv("./data/23-03-30_16-41-54_results.tsv",delimiter='\t')
+    df_cm = pd.read_csv("./tests/data/23-03-30_16-21-39_results.tsv",delimiter='\t')
+    df_ru = pd.read_csv("./tests/data/23-03-30_16-41-54_results.tsv",delimiter='\t')
     #TODO --> add tsv of romp
-    df =pd.concat([df1, df2], ignore_index=True)
-    df = df.loc[df["Model_name"] == model]
-    df_selected = df[["ModelChecker",feature]]
-    box_plot(df_selected, model, feature)
+    df = pd.concat([df_cm, df_ru], ignore_index=True)
+    models = set(df['Model_name'])
+    # {} nested for how many qualifiers you want to have
+    box_plot({x:{m:dfx.query(f"Model_name=='{m}'").Time.to_numpy() for m in ['ruMI','german','n_peterson_bug1']} for x,dfx in [('cmurphi',df_cm),('rumur',df_ru)]}
+            )
     
+
+
+#%%
 if __name__ == "__main__":
     main()
