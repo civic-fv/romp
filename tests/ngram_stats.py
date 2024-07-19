0096468d4f3bbf266d74eff139a1794495bb3ad0
Shambhavi Sudarsan <43706184+shambhavisudarsan@users.noreply.github.com>
2024-07-19 15:54:25 -0600

    Update ngram_stats.py
    Updated and incorporated command-line arguments.

--- tests/ngram_stats.py
+++ tests/ngram_stats.py
@@ -14,10 +14,10 @@ def create_rule_name(rule) -> str:
     sep = ""
 
     for q in rule.get('quantifiers',[]):
-        if "scalarset" in q['type']:
+        if "scalarset" in q['value']['type']:
             res += sep + q['value']['value'][6:]
-        elif "range" in q['type']:
-            res += sep + q['value']['value'] #TODO check how ranges are output as quantifiers
+        elif "range" in q['value']['type']:
+            res += sep + str(q['value']['value']['value']) #TODO check how ranges are output as quantifiers
         sep = ','
     return res + ')'
 
@@ -30,14 +30,14 @@ def _do_process(path: str):
     with open(path, 'r') as file:
         trace_file = json.load(file)
         total_rule_count = trace_file['metadata']['total-rule-count']
-
+        
         if not trace_file['metadata']['simple-trace']:
             rule_extractor = (lambda x: create_rule_name(x.get('rule', {})))
         else:
             rule_extractor = (lambda x: x.get('rule','<UNKNWON>'))
         for trace in trace_file['trace']:
             rule_type = trace['$type']
-            rule_name = rule_extractor(trace) #this is designed for traces with simple trace disabled
+            rule_name = rule_extractor(trace) #this is designed for traces with simple trace disabled 
 
             if rule_type in {'rule-hit', 'rule-miss'}:
                 rule_sequence.append(rule_name)
@@ -48,19 +48,18 @@ def _do_process(path: str):
                     rule_misses[rule_name] += 1
                     misses += 1
 
-        return rule_sequence, rule_hits, rule_misses, hits, misses, total_rule_count
+    return rule_sequence, rule_hits, rule_misses, hits, misses, total_rule_count
 
 def calculate_ngrams(sequence, n):
     ngram_seq = deque(maxlen=n)
     ngrams = defaultdict(int)
 
-    for i in range(len(sequence)-n):
-        ngrams[tuple(sequence[i:i+n])] += 1
+    for i in range(len(sequence) - n):
+        ngram = tuple(sequence[i:i + n])
+        if '<UNKNOWN>' not in ngram[0]:
+            ngrams[ngram] += 1
 
-    # Filter out n-grams that start with 'unknown'
-    filtered_ngrams = {k: v for k, v in ngrams.items() if '<UNKNOWN>' in k[0]}
-
-    return filtered_ngrams
+    return ngrams
 
 def plot_histogram(data, title, xlabel, ylabel, filename, top_n=20):
     # Limit to top_n data points for readability
@@ -84,14 +83,14 @@ def plot_histogram(data, title, xlabel, ylabel, filename, top_n=20):
     fig.show()
 
 
-def main(n):
-    json_files = glob.glob('*.json')
+def get_stats(n,dir):
+    json_files = glob.glob(f"{dir}/*.json")
 
     if not json_files:
         print("No JSON files found in the current directory.")
         sys.exit(1)
-
-    all_rule_sequences = []
+    
+    all_rule_sequences = [] 
     all_filtered_ngrams = defaultdict(int)
     total_count = 0
     walk_count = 0
@@ -101,9 +100,9 @@ def main(n):
         rule_sequence, rule_hits, rule_misses, hits, misses, n_rules = _do_process(json_file)
         all_rule_sequences.append(rule_sequence)
 
-    expected_ngrams: int = (fact(n_rules)//(fact(n)*fact(n_rules-n))  # n total, r sample size  -> n! / (r!((n-r)!))
-
-
+    expected_ngrams: int = (fact(n_rules)//(fact(n)*fact(n_rules-n)))  # n total, r sample size  -> n! / (r!((n-r)!))
+    
+    
     for sequence in all_rule_sequences:
         filtered_ngrams = calculate_ngrams(sequence, n)
         for k, v in filtered_ngrams.items():
@@ -114,17 +113,22 @@ def main(n):
 
     for ngram, count in avg_ngram_freqs.items():
         print(f'{ngram}: {count}')
-
+    
     plot_histogram(avg_ngram_freqs, 'Top Average N-gram Frequencies Across All Walks', 'Average Frequency', 'N-grams', 'avg_ngram_frequencies.png', top_n=20)
 
 
     # plot_histogram(all_filtered_ngrams, 'N-gram Frequencies', 'Frequency', 'N-grams', 'ngram_frequencies.png', top_n=20, min_freq=5)
     # plot_histogram(avg_ngram_freqs, 'Average N-gram Frequencies Across All Walks', 'Average Frequency', 'N-grams', 'avg_ngram_frequencies.png', top_n=20, min_freq=0.1)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./trace_statistics.py <n>")
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: ./trace_statistics.py <dir> <n>")
         sys.exit(1)
 
-    n = int(sys.argv[1])
-    main(n)
+    n = int(sys.argv[2])
+    dir = sys.argv[1]
+    get_stats(n,dir)
+
+
+if __name__ == "__main__":
+    main()
